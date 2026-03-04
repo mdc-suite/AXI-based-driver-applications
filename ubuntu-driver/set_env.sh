d622280ee3e9f67a0167adf234b6b52f9a2c45ac
Marco Fois <106070712+marcofoiss@users.noreply.github.com>
2026-03-04 17:49:28 +0100

    Update set_env.sh
    Set up environment for AXI-based drivers installation and loading.

--- ubuntu-driver/set_env.sh
+++ ubuntu-driver/set_env.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
+set -e
 
+git clone https://github.com/GiuseppeSatta/AXI-based-drivers.git
+cd AXI-based-drivers
+
+sudo bash install_libs.sh
+
+cd axidma-driver
+make
+sudo mkdir -p /lib/modules/$(uname -r)/extra
+sudo cp axidma.ko /lib/modules/$(uname -r)/extra/
+sudo depmod -a
+echo axidma | sudo tee /etc/modules-load.d/axidma.conf
+sudo modprobe axidma
+lsmod | grep axidma
